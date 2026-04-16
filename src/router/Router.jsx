c26ed5887a6841ad294b923e6f2c2dff9031e778
us324034@gmail.com <us324034@gmail.com>
2026-04-16 17:35:56 +0500

    router change

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,7 +3,7 @@ import Home from "../pages/Home"
 import Collection from "../pages/Collection"
 import Cart from "../pages/Cart"
 import Login from "../pages/Login"
-import { RedirectToSignIn, SignOutButton } from "@clerk/react"
+import { RedirectToSignIn } from "@clerk/react"
 import DetailPage from "../pages/DetailPage"
 import Contact from "../pages/Contact"
 
@@ -14,15 +14,32 @@ const Router = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-            <Route index element={<Home />} />
-            <Route path="/collection" element={<Collection />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/product/:id" element={<DetailPage />} />
+            <Route index element={<>
+                <SignedIn> <Home /> </SignedIn>
+                <SignedOut> <RedirectToSignIn /> </SignedOut>
+            </>} />
+
+            <Route path="/collection" element={<>
+                <SignedIn> <Collection /> </SignedIn>
+                <SignedOut> <RedirectToSignIn /> </SignedOut>
+            </>} />
+
+            <Route path="/contact" element={<>
+                <SignedIn> <Contact /> </SignedIn>
+                <SignedOut> <RedirectToSignIn /> </SignedOut>
+            </>} />
+
+            <Route path="/cart" element={<>
+                <SignedIn> <Cart /> </SignedIn>
+                <SignedOut> <RedirectToSignIn /> </SignedOut>
+            </>} />
+
+            <Route path="/product/:id" element={<>
+                <SignedIn> <DetailPage /> </SignedIn>
+                <SignedOut> <RedirectToSignIn /> </SignedOut>
+            </>} />
             <Route path="*" element={
-                <SignOutButton>
-                    <RedirectToSignIn />
-                </SignOutButton>
+                <RedirectToSignIn />
             } />
         </Routes>
 
